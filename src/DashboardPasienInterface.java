dc0928f0774e16eaf15e83bdf6b0f59083094670
cutekarensiana06-ux <cutekarensiana06@gmail.com>
2026-07-01 08:24:53 +0700

    Delete src directory

--- src/DashboardPasienInterface.java
+++
@@ -1,5 +0,0 @@
-public interface DashboardPasienInterface {
-    void muatJadwal();          // Method untuk memuat jadwal terapi
-    void muatRiwayat();         // Method untuk memuat riwayat medis
-    void muatProgramLatihan();  // Method untuk memuat instruksi latihan
-}
